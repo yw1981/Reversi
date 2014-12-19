c1c2fd48241f845f43c6d66f07e3da1ca1cc809a
Purnima01 <Purnima01@users.noreply.github.com>
2014-12-18 19:54:04 -0500

    Changed audio

--- game.js
+++ game.js
@@ -7,7 +7,7 @@ angular.module('myApp', ['ngTouch'])
 
       var moveAudioB = new Audio('audio/move.mp3');
       moveAudioB.load();
-      var moveAudioW = new Audio('audio/move1.mp3');
+      var moveAudioW = new Audio('audio/move.mp3');
       moveAudioW.load();
 
     function sendComputerMove(){
